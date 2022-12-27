f3e47a279433d2ef614b5ff35e26542c1246478d
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-12-26 21:38:36 -0300

    Implement Entity Enrollment

---
+++ src/main/java/com/fronchak/DSLearn/entities/Enrollment.java
@@ -0,0 +1,104 @@
+package com.fronchak.DSLearn.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import com.fronchak.DSLearn.entities.pk.EnrollmentPK;
+
+@Entity
+@Table(name = "enrollment")
+public class Enrollment {
+
+	@EmbeddedId
+	private EnrollmentPK id = new EnrollmentPK();
+	
+	@Column(name = "enroll_moment", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant enrollMoment;
+	
+	@Column(name = "refund_moment", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant refundMoment;
+	private boolean available;
+	private boolean onlyUpdate;
+	
+	public Enrollment( ) {}
+	
+	public Enrollment(User user, Offer offer) {
+		id.setUser(user);
+		id.setOffer(offer);
+	}
+	
+	public void setId(User user, Offer offer) {
+		id.setUser(user);
+		id.setOffer(offer);
+	}
+	
+	public User getStudent() {
+		return id.getUser();
+	}
+	
+	public void setStudent(User student) {
+		id.setUser(student);
+	}
+	
+	public Offer getOffer() {
+		return id.getOffer();
+	}
+	
+	public void setOffer(Offer offer) {
+		id.setOffer(offer);
+	}
+ 	
+	public Instant getEnrollMoment() {
+		return enrollMoment;
+	}
+
+	public void setEnrollMoment(Instant enrollMoment) {
+		this.enrollMoment = enrollMoment;
+	}
+	
+	public Instant getRefundMoment() {
+		return refundMoment;
+	}
+	
+	public void setRefundMoment(Instant refundMoment) {
+		this.refundMoment = refundMoment;
+	}
+	
+	public boolean isAvailable() {
+		return available;
+	}
+	
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
+	
+	public boolean isOnlyUpdate() {
+		return onlyUpdate;
+	}
+	
+	public void setOnlyUpdate(boolean onlyUpdate) {
+		this.onlyUpdate = onlyUpdate;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enrollment other = (Enrollment) obj;
+		return Objects.equals(id, other.id);
+	}
+}
